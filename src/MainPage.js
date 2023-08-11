319ad4d51cc3e2947a57696e058bcf4e05767c45
Aser-Ghobara <113632948+Aser-Ghobara@users.noreply.github.com>
2023-08-11 21:40:33 +0300

    ProfilepicCode

--- src/MainPage.js
+++ src/MainPage.js
@@ -23,11 +23,20 @@ const MainPage = () => {
     <section id="home">
       <div className="home2">
         <div className="content">
+        <div className="container2">
           <div className={`hero ${animate ? 'animate' : ''}`}>
+          <div class="image profile">          
+          <img src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                    height="400px" alt="profile pic" />        
+          </div>
+          <div class="text">
             <h1>{animatedName}</h1>
             <p>{animatedDescription}</p>
           </div>
-
+          </div>  
+        </div>
+        
+        <div className="socials">
           <ul className="icons">
             <li>
               <a href="https://www.linkedin.com/in/aser-ghobara-0b1b3b1b0/" target="_blank" rel="noreferrer">
@@ -41,7 +50,25 @@ const MainPage = () => {
             </li>
           </ul>
         </div>
+        </div>
+        
 
+        <div class="hero flex items-centre justify-between">
+          <div class="left flex-1 justify-center">
+            <img src="https://i.imgur.com/JPUVPVl.png" />
+          </div>
+
+          <div class="right flex-1">
+            <h6>Aser Ghobara</h6>
+            <h1>I am a Software Engineer</h1>
+
+            <p>
+              I am a software engineer based in Canada. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences. 
+            </p>
+            <div><button class="btn btn-secondary">DOWNLOAD CV</button></div>
+        </div>
+
+        </div>
         <div className="buttons">
           <button className="btnproject">
             <Link to="/projects">Projects</Link>
